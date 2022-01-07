e012876b4559fee61269a2f2a55321795e3084fd
osama1707 <85078282+osama1707@users.noreply.github.com>
2022-01-07 22:08:28 +0200

    Add files via upload

---
+++ Quantity_typeModel.php
@@ -0,0 +1,28 @@
+<?php
+
+class QuantityType
+{
+	public $id;
+	public $type;
+	public $pr_id;
+
+	function __construct($id)
+	{
+        define('SERVER', 'localhost');
+        define('USERNAME', 'root');
+        define('PASSWORD', '');
+        define('DB_NAME', 'charity_org_db');
+
+        $con = mysqli_connect(SERVER, USERNAME, PASSWORD, DB_NAME);
+		$sql ="select * from quantity_type where id=$id";
+		$userDataSet =mysqli_query($con,$sql);
+		if ($row= mysqli_fetch_array($userDataSet)) 
+        {
+			$this->id =$row["id"];
+            $this->type=$row["type"];			
+		}
+	}
+}
+
+
+?>
